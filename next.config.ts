99c3170c20d5a3f05a79c07ee119ef244506555e
Shinyn <mattias.alm@hotmail.com>
2026-03-17 15:58:37 +0100

    chore: update deprecated pattern

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,12 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ['picsum.photos'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+      },
+    ],
   },
   /* config options here */
   reactCompiler: true,
