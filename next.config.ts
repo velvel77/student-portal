322e5155ad6613ad9a38e1e0635f37d55bd73bc7
Shinyn <mattias.alm@hotmail.com>
2026-03-17 15:54:05 +0100

    chore: add picsum domain to config

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
+  images: {
+    domains: ['picsum.photos'],
+  },
   /* config options here */
   reactCompiler: true,
 };
