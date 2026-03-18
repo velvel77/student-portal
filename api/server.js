5d87aab0ae573c56551b2e87a13abff08e24102e
Velvet <velvet.pinkish@gmail.com>
2026-03-18 17:05:37 +0100

    Feature: Refactored get and post into studentsRouter

--- api/server.js
+++ api/server.js
@@ -1,40 +1,11 @@
 import express from "express";
-import { pool } from "./db.js";
+import studentsRouter from './routes/students.js';
 
 const app = express();
 const PORT = 5000;
 
 app.use(express.json());
-
-app.get("/students", async (req, res) => {
-	try {
-		const result = await pool.query(
-			"SELECT * FROM student ORDER BY id ASC",
-		);
-		res.json(result.rows);
-	} catch (err) {
-		console.error(err);
-		res.status(500).send("Server error");
-	}
-});
-
-app.post("/students", async (req, res) => {
-	const { name, portfolio_link, tech_stack, picture, bio } = req.body;
-
-	try {
-		const result = await pool.query(
-			`INSERT INTO student (name, portfolio_link, tech_stack, picture, bio)
-       VALUES ($1, $2, $3, $4, $5)
-       RETURNING *`,
-			[name, portfolio_link, tech_stack, picture, bio],
-		);
-
-		res.json(result.rows[0]);
-	} catch (err) {
-		console.error(err);
-		res.status(500).send("Insert failed");
-	}
-});
+app.use('/students', studentsRouter);
 
 app.listen(PORT, () => {
 	console.log(`Server running on http://localhost:${PORT}`);
