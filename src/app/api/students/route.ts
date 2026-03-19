7b9de631e47e46bc99dda3c1a05188198aac388e
henrietta jeansson <henriettajeansson@gmail.com>
2026-03-19 09:28:52 +0100

    feat: changed ports

--- src/app/api/students/route.ts
+++ src/app/api/students/route.ts
@@ -1,5 +1,5 @@
 export async function GET() {
-	const res = await fetch("http://localhost:5001/students");
+	const res = await fetch("http://localhost:5000/students");
 	const data = await res.json();
 
 	return Response.json(data);
